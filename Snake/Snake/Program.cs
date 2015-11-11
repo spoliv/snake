c0059e04196d297410ec56b18dd0b97e60eb679e
spoliv <spoliv@rambler.ru>
2015-11-11 15:41:01 +0300

    Class Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,25 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
 
-            Draw(x1, y1, sim1);
+            p1.Draw(p1.x, p1.y, p1.sym);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sim2 = '#';
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
 
-            Draw(x2, y2, sim2);
+            p2.Draw(p2.x, p2.y, p2.sym);
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
-        }
     }
 }
