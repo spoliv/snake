274e4619d9ce6bffbabf1261d130fa1c41ebbf4c
spoliv <spoliv@rambler.ru>
2015-11-12 18:31:58 +0300

    Исправления в ClassList

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,14 +15,14 @@ namespace Snake
             p1.y = 3;
             p1.sym = '*';*/
 
-            p1.Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             /*p2.x = 4;
             p2.y = 5;
             p2.sym = '#';*/
 
-            p2.Draw(p2.x, p2.y, p2.sym);
+            p2.Draw();
 
             Point p3 = new Point(6, 8, '&');
             Point p4 = new Point(9, 10, '@');
