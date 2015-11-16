691505c7850af7ef6b5d8a8fb5022726b2897a5d
spoliv <spoliv@rambler.ru>
2015-11-16 17:42:24 +0300

    Несущественные изменения

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,7 +26,7 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-
+           
             Console.ReadLine();
 
         }
