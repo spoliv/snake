f8d206c74109c08507cbdc385b846f737e01fd86
spoliv <spoliv@rambler.ru>
2015-11-24 14:17:41 +0300

    Небольшое добавление

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -29,8 +29,8 @@ namespace Snake
             while (true)
             {
                 if(walls.IsHit(snake) || snake.IsHitTail())
-                {
-                    break;
+                {                 
+                    break;                  
                 }
                 if(snake.Eat(food))
                 {
@@ -52,13 +52,28 @@ namespace Snake
                 }
 
             }
-          
-            /*Console.ReadLine();*/
+            WriteGameOver();
+            Console.ReadLine();
         }
 
-        /*static void Draw(Figura figura)
+        static void WriteGameOver()
         {
-            figura.Draw();
-        }*/
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("==============================================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Спресов О. под Руководством Картавца Е.", xOffset + 2, yOffset++);
+            WriteText("Специально для GeekBrains", xOffset + 1, yOffset++);
+            WriteText("==============================================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
     }
 }
