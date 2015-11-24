2ab38af55144b2e63646f5815606f4d2a6ff1b1e
spoliv <spoliv@rambler.ru>
2015-11-24 14:25:33 +0300

    Еще изменения

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -62,12 +62,14 @@ namespace Snake
             int yOffset = 8;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.SetCursorPosition(xOffset, yOffset++);
-            WriteText("==============================================", xOffset, yOffset++);
+            WriteText("==============================", xOffset, yOffset++);
             WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
             yOffset++;
-            WriteText("Автор: Спресов О. под Руководством Картавца Е.", xOffset + 2, yOffset++);
+            WriteText("Автор: Спресов О.", xOffset + 2, yOffset++);
+            WriteText("под Руководством Картавца Е.", xOffset + 2, yOffset++);
+            yOffset++;
             WriteText("Специально для GeekBrains", xOffset + 1, yOffset++);
-            WriteText("==============================================", xOffset, yOffset++);
+            WriteText("==============================", xOffset, yOffset++);
         }
 
         static void WriteText(String text, int xOffset, int yOffset)
